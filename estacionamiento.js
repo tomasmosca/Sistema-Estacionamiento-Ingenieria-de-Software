2b1df524b17d7cdf577d5cf4b2d880848128fd07
tomasmosca <tomasmoscarelli@hotmail.com>
2021-06-10 23:35:11 -0300

    agregado consulta estacionamiento

---
+++ estacionamiento.js
@@ -0,0 +1,10 @@
+function estacionamiento(codigoUbicacion,zona,ubicacion,disponibilidad,patente){
+	this.codigoUbicacion = codigoUbicacion;
+	this.zona = zona;
+	this.disponibilidad = disponibilidad;
+	this.patente = patente;
+	this.ubicacion = ubicacion;
+	//this.costo;
+	this.horaInicio = 11;
+	this.horaFin;
+}
---
+++ zona.js
@@ -0,0 +1,7 @@
+function zona(color, limites)
+{
+	this.color = color;
+	this.limites = limites;
+	//this.precio;
+	//this.hora;
+}
